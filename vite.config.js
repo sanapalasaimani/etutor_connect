c645341bf1e9001d8252518b474624cf96ff45ca
saimani <sanapalasaimani1@gmail.com>
2026-04-11 11:42:22 +0530

    rewrite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import { resolve } from 'path'
+import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
+import { resolve } from 'path';
 
 export default defineConfig({
+  plugins: [
+    tailwindcss(),
+  ],
   build: {
     rollupOptions: {
       input: {
@@ -16,4 +20,4 @@ export default defineConfig({
       }
     }
   }
-})
+});
