f98a9c2681b54b867c650d6e1e2fc762fcc2b33e
saimani <sanapalasaimani1@gmail.com>
2026-04-11 11:35:35 +0530

    fix routing and build

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
-import { defineConfig } from 'vite';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
-});
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        login: resolve(__dirname, 'login.html'),
+        signup: resolve(__dirname, 'signup.html'),
+        blocked: resolve(__dirname, 'blocked.html'),
+        updatePassword: resolve(__dirname, 'update-password.html'),
+        student: resolve(__dirname, 'student/index.html'),
+        tutor: resolve(__dirname, 'tutor/index.html'),
+        admin: resolve(__dirname, 'admin/index.html')
+      }
+    }
+  }
+})
